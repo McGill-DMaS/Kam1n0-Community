McGill-DMaS/Kam1n0-Community

commit 089f0b55b841cf62a07b997fe4d8d347142d49ed

earlier filtering of 'same binary' blocks

kam1n0/kam1n0-clone/src/main/java/ca/mcgill/sis/dmas/kam1n0/problem/clone/detector/kam/FunctionSubgraphDetector.java
@@ -161,6 +161,7 @@ public class FunctionSubgraphDetector extends FunctionCloneDetector implements S
 			// convert (tar, src, score) to (srcfuncid, (tar, src, score))
 			JavaRDD<Tuple2<Long, Tuple3<Block, Block, Double>>> b_to_b = indexer//
 					.queryAsRdds(rid, vBlks, links, topK)//
+					.filter(tuple -> !avoidSameBinary || (tuple._2().binaryId != function.binaryId))
 					.map(tuple -> new Tuple2<>(tuple._2().functionId, tuple));
 
 			// logger.info("b_to_b {}", b_to_b.count());
@@ -193,8 +194,7 @@ public class FunctionSubgraphDetector extends FunctionCloneDetector implements S
 			}
 		}
 
-		return results.stream().filter(fce -> !avoidSameBinary || (fce.binaryId != function.binaryId))
-				.collect(Collectors.toList());
+		return results;
 	}
 
 	public String params() {

commit 5e820aeb0b6c7ea34a304bbd67cfb0d6a986a1f5

fix bug introduced in 089f0b55b841cf62a07b997fe4d8d347142d49ed
- clones for single-block functions were not filtered for "avoid same binary" anymore.

kam1n0/kam1n0-clone/src/main/java/ca/mcgill/sis/dmas/kam1n0/problem/clone/detector/kam/FunctionSubgraphDetector.java
@@ -133,24 +133,25 @@ public class FunctionSubgraphDetector extends FunctionCloneDetector implements S
 			// if (false) {
 			// indexer.queryAsRdd(target, threshold, topK)
 			List<Tuple2<Block, Double>> blks = indexer.query(rid, vBlks.get(0), threshold, topK);
-			results = blks.stream().map(ent -> {
-				FunctionCloneEntry entry = new FunctionCloneEntry();
-				entry.binaryId = ent._1.binaryId;
-				entry.binaryName = ent._1.binaryName;
-				entry.functionId = ent._1.functionId;
-				entry.functionName = ent._1.functionName;
-				entry.similarity = ent._2;
-				Tuple3<Long, Long, Double> tp = new Tuple3<Long, Long, Double>(vBlks.get(0).blockId, ent._1.blockId,
-						ent._2);
-				entry.clonedParts.add(new HashSet<>(Arrays.asList(tp)));
-				return entry;
-			}).collect(Collectors.toList());
-
+			results = blks.stream()
+					.filter(ent -> !avoidSameBinary || ent._1.binaryId != function.binaryId)
+					.map(ent -> {
+						FunctionCloneEntry entry = new FunctionCloneEntry();
+						entry.binaryId = ent._1.binaryId;
+						entry.binaryName = ent._1.binaryName;
+						entry.functionId = ent._1.functionId;
+						entry.functionName = ent._1.functionName;
+						entry.similarity = ent._2;
+						Tuple3<Long, Long, Double> tp = new Tuple3<Long, Long, Double>(vBlks.get(0).blockId, ent._1.blockId,
+								ent._2);
+						entry.clonedParts.add(new HashSet<>(Arrays.asList(tp)));
+						return entry;
+					}).collect(Collectors.toList());
 		} else {
 
 			// convert (tar, src, score) to (srcfuncid, (tar, src, score))
-			JavaRDD<Tuple2<Long, Tuple3<Block, Block, Double>>> b_to_b = indexer//
-					.queryAsRdds(rid, vBlks, links, topK)//
+			JavaRDD<Tuple2<Long, Tuple3<Block, Block, Double>>> b_to_b = indexer
+					.queryAsRdds(rid, vBlks, links, topK)
 					.filter(tuple -> !avoidSameBinary || (tuple._2().binaryId != function.binaryId))
 					.map(tuple -> new Tuple2<>(tuple._2().functionId, tuple));
 