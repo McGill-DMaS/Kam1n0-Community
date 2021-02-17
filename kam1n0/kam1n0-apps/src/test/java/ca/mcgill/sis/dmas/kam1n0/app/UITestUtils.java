786c317c1784926ad3de40fc8c246a5b88e96a6e
Emmanuel Giasson <emmanuel.giasson@ca.thalesgroup.com>
2021-02-17 16:55:33 -0500

    make UITest launchable with JDK 11

--- kam1n0/kam1n0-apps/src/test/java/ca/mcgill/sis/dmas/kam1n0/app/UITestUtils.java
+++ kam1n0/kam1n0-apps/src/test/java/ca/mcgill/sis/dmas/kam1n0/app/UITestUtils.java
@@ -29,9 +29,30 @@ public class UITestUtils {
 		ProcessBuilder builder = new ProcessBuilder();
 		String binLocation = System.getProperty("output.dir");
 		log("Bin directory is on {}", binLocation);
-		appProcess = builder.command("java", "-Xmx6G", "-Xss4m", "-Dkam1n0.ansi.enable=false",
-				"-Dkam1n0.spring.popup=false", "-Dlogging.level.org.springframework=INFO", "-jar",
-				binLocation + "/kam1n0-server.jar", "--start", "kam1n0.data.path", dataPath.getAbsolutePath())
+		appProcess = builder.command("java",
+				"-Xmx6G",
+				"-Xss4m",
+				"\"-Dkam1n0.ansi.enable=false\"",
+				"\"-Dkam1n0.spring.popup=false\"",
+				"\"-Dlogging.level.org.springframework=INFO\"",
+				"\"-Djdk.attach.allowAttachSelf=true\"",
+				"--add-exports=java.base/jdk.internal.misc=ALL-UNNAMED",
+				"--add-exports=java.base/jdk.internal.ref=ALL-UNNAMED",
+				"--add-exports=java.base/sun.nio.ch=ALL-UNNAMED",
+				"--add-exports=java.management.rmi/com.sun.jmx.remote.internal.rmi=ALL-UNNAMED",
+				"--add-exports=java.rmi/sun.rmi.registry=ALL-UNNAMED",
+				"--add-exports=java.rmi/sun.rmi.server=ALL-UNNAMED",
+				"--add-exports=java.sql/java.sql=ALL-UNNAMED",
+				"--add-opens=java.base/java.lang.module=ALL-UNNAMED",
+				"--add-opens=java.base/jdk.internal.loader=ALL-UNNAMED",
+				"--add-opens=java.base/jdk.internal.ref=ALL-UNNAMED",
+				"--add-opens=java.base/jdk.internal.reflect=ALL-UNNAMED",
+				"--add-opens=java.base/jdk.internal.math=ALL-UNNAMED",
+				"--add-opens=java.base/jdk.internal.module=ALL-UNNAMED",
+				"--add-opens=java.base/jdk.internal.util.jar=ALL-UNNAMED",
+				"--add-opens=jdk.management/com.sun.management.internal=ALL-UNNAMED",
+				"-jar",	binLocation + "/kam1n0-server.jar",
+				"--start", "kam1n0.data.path", dataPath.getAbsolutePath())
 				.inheritIO().start();
 	}
 
