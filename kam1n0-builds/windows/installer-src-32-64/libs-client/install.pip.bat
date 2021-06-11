73d5bd3e8c036d121d13b9f473dcd5de422559c9
Steven Ding <steven-hh-ding@users.noreply.github.com>
2021-06-10 22:03:50 -0400

    Update install.pip.bat
    fixing cef installation bug (wrong python path)

--- kam1n0-builds/windows/installer-src-32-64/libs-client/install.pip.bat
+++ kam1n0-builds/windows/installer-src-32-64/libs-client/install.pip.bat
@@ -3,16 +3,15 @@ setlocal
 
 for %%i in ("python.exe") do set PY_ENV_EXE=%%~$PATH:i 
 FOR %%a IN ("%PY_ENV_EXE:~0,-1%") DO SET PY_ENV_HOME=%%~dpa
+echo hello
 echo %PY_ENV_HOME%
 echo %PY_ENV_EXE%
 
-if "%~1"=="PY_ENV_DEFAULT" (
-	SET PYTHON_EXE=%PY_ENV_EXE%
-	SET PYTHON_PIPY=%PY_ENV_HOME%\Scripts\pip.exe
-) else (
-	SET PYTHON_EXE=%1\python.exe
-	SET PYTHON_PIPY=%1\Scripts\pip.exe
-)
+
+SET PYTHON_EXE=%PY_ENV_EXE%
+SET PYTHON_PIPY=%PY_ENV_HOME%\Scripts\pip.exe
+echo %PYTHON_EXE%
+echo %PYTHON_PIPY%
 
 if not exist %PYTHON_PIPY% (
     echo "%PYTHON_EXE%" get-pip.py --no-index --find-links=.
